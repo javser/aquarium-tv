737eab21f1b5099cafca64dc065f748bcd9dcb5f
Void (Serg Zh) <34738549+javser@users.noreply.github.com>
2026-08-29 20:14:39 +0300

    Create WhiteBackgroundRemover.kt

---
+++ app/src/main/java/ru/javser/aquarium/WhiteBackgroundRemover.kt
@@ -0,0 +1,43 @@
+package ru.javser.aquarium
+
+import android.graphics.Bitmap
+import android.graphics.Color
+
+object WhiteBackgroundRemover {
+
+    fun removeWhite(src: Bitmap): Bitmap {
+        val bmp = src.copy(Bitmap.Config.ARGB_8888, true)
+        val w = bmp.width
+        val h = bmp.height
+        val pixels = IntArray(w * h)
+        bmp.getPixels(pixels, 0, w, 0, w, h)
+
+        val isWhite = BooleanArray(w * h) { i ->
+            val c = pixels[i]
+            minOf(Color.red(c), Color.green(c), Color.blue(c)) > 215
+        }
+        val visited = BooleanArray(w * h)
+        val queue = IntArray(w * h)
+        var head = 0; var tail = 0
+
+        fun seed(x: Int, y: Int) {
+            val i = y * w + x
+            if (isWhite[i] && !visited[i]) { visited[i] = true; queue[tail++] = i }
+        }
+        for (x in 0 until w) { seed(x, 0); seed(x, h - 1) }
+        for (y in 0 until h) { seed(0, y); seed(w - 1, y) }
+
+        while (head < tail) {
+            val i = queue[head++]
+            val x = i % w; val y = i / w
+            pixels[i] = Color.TRANSPARENT
+            if (x > 0)     { val j = i - 1; if (isWhite[j] && !visited[j]) { visited[j] = true; queue[tail++] = j } }
+            if (x < w - 1) { val j = i + 1; if (isWhite[j] && !visited[j]) { visited[j] = true; queue[tail++] = j } }
+            if (y > 0)     { val j = i - w; if (isWhite[j] && !visited[j]) { visited[j] = true; queue[tail++] = j } }
+            if (y < h - 1) { val j = i + w; if (isWhite[j] && !visited[j]) { visited[j] = true; queue[tail++] = j } }
+        }
+
+        bmp.setPixels(pixels, 0, w, 0, w, h)
+        return bmp
+    }
+}
